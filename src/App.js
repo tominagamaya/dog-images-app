ca15b686b6098e1b10fbe1ccef617436894a57ff
maya.tominaga <maya.tominaga@avanade.com>
2022-01-18 16:55:00 +0900

    update components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
 import './App.css';
 
+function Header() {
+  return (
+    <header className="App-header">
+        <h1>Dog Images</h1>
+    </header>
+  )
+}
+
+function Image() {
+  return (
+    <figure>
+      <img
+        src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
+        alt="cute dog"
+      ></img>
+    </figure>
+  )
+}
+
+function Main() {
+  return (
+    <main>
+      <section>
+        <Image />
+      </section>
+    </main>
+  )
+}
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>Dog images are retrieved from Dog API</p>
+      <p>
+        <a href="https://dog.ceo/dog-api/about">Donate to Dog API</a>
+      </p>
+    </footer>
+  )
+}
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <h1>Dog Images</h1>
-      </header>
-      <main>
-        <section>
-          <figure>
-            <img
-              src="https://images.dog.ceo/breeds/shiba/shiba-8.jpg"
-              alt="cute dog"
-            ></img>
-          </figure>
-        </section>
-      </main>
-      <footer className="footer">
-        <p>Dog images are retrieved from Dog API</p>
-        <p>
-          <a href="https://dog.ceo/dog-api/about">Donate to Dog API</a>
-        </p>
-      </footer>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
